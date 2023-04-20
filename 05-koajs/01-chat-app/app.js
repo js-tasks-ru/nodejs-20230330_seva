09f667a34691bb6da5142e16b0ca751036aa9e0a
Vsevolod <helloalicesmith@gmail.com>
2023-04-20 12:53:37 +0300

    5st task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,29 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const arr = []
+
 router.get('/subscribe', async (ctx, next) => {
+    const res = await new Promise((res, rej) => {
+        arr.push(res)
+    })
+
+    ctx.body = res
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message
+
+    if (!message) {
+        return
+    }
+
+    arr.forEach((it) => {
+        it(message)
+    })
+    arr = []
+
+     ctx.res.statusCode = 201;
 });
 
 app.use(router.routes());
